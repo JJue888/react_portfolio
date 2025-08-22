12b30a3d5475d9b09204ab5c632c0ee4dd3273de
JJue888 <joshuajue888@gmail.com>
2025-08-21 23:10:32 -0700

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import RootLayout from "./pages/Root";
+import HomePage from "./pages/Home";
+import ProjectsPage from "./pages/Projects";
+import AboutPage from "./pages/About";
+import ProjectDetailsPage from "./pages/ProjectDetails";
+import ExperiencesPage from "./pages/Experiences";
+import ExperienceDetailsPage from "./pages/ExperienceDetails";
+import ContactPage from "./pages/Contact";
+
+const router = createBrowserRouter([
+      {path: "/react_portfolio/", element: <RootLayout />, children: [
+          { index: true, element: <HomePage /> },
+              {path: "about", element: <AboutPage />},
+          {path: 'projects', element: <ProjectsPage />, children: [
+              { path:":projectTitle", element: <ProjectDetailsPage />},
+            ]},
+              {path:'experiences', element: <ExperiencesPage />, children: [
+                      {path:":experienceTitle", element: <ExperienceDetailsPage />},
+                  ]},
+              {path:'contact', element: <ContactPage />}
+        ]},
+    ]
+)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
