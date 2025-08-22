4aba1dc621264a35e6b689afe5b43256e1703036
JJue888 <joshuajue888@gmail.com>
2025-08-22 15:09:20 -0700

    Home page text update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
+import classes from './Home.module.css'
+
 function HomePage() {
     return (
         <>
             <h1>Welcome to my page</h1>
+            <p>Feel free to look around the site</p>
+            <p>I built this using React and React Router for the routing</p>
         </>
     )
 }
