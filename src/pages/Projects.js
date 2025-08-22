1c380e7289863bb70d20b12a1efd3c8ec257c8c1
JJue888 <joshuajue888@gmail.com>
2025-08-22 15:11:35 -0700

    Project formatting

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,18 +1,19 @@
 import { PROJECT_DATA } from "../data/projects";
 import WorkDisplay from "../components/WorkDisplay";
 import {Link} from "react-router-dom";
+import classes from "./Projects.module.css"
 
 function ProjectsPage() {
     return (
         <>
             <div>
-                <header className="w-full py-6 text-center  dark:bg-gray-950">
-                    <h1 className="text-3xl font-bold text-black dark:text-white">Projects</h1>
+                <header className={classes.head}>
+                    <h1 className="">Projects</h1>
                 </header>
-                <div>
-                    <ul className="flex flex-wrap gap-4 justify-evenly pb-2">
+                <div className={classes.projects}>
+                    <ul>
                         {PROJECT_DATA.map((project) => (
-                            <li key={project.title} className="w-[40vw] bg-gray-950 p-4 rounded-lg shadow-md">
+                            <li key={project.title} className={classes.projectContainer}>
                                 <Link to={project.title}>
                                     <WorkDisplay {...project} />
                                 </Link>
