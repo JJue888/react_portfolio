a62ba4abb13dd02dc388366d63a72636e8a98278
JJue888 <joshuajue888@gmail.com>
2025-08-22 21:48:08 -0700

    Added ccdc image

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,11 +1,12 @@
 import webmasterImage from "../assets/projects/webmaster1.png"
+import ccdcImage from "../assets/projects/ccdcCareerPage.png"
 
 export const EXPERIENCE_DATA = [
 
     {
         title: 'Chinatown Community Development Center Intern',
         description: [],
-        image: [],
+        image: [ccdcImage,],
     },
 
     {
@@ -15,7 +16,7 @@ export const EXPERIENCE_DATA = [
             "Through this role, I have experimented with different ways of displaying information and trying to improve user experience. This has allowed me to have a more in-depth understanding of what goes into creating and maintaining a website.",
             "Recently, I was tasked with finding a way to display all of the past composites to allow alumni to view them. To create this page, I developed different methods in JavaScript to build each part of the page. This allowed me to keep the code organized by having each method build off the other and helped me learn more about JavaScript. To display the composite in full screen, I used modal contents with onclick functions.",
         ],
-        image: [webmasterImage],
+        image: [webmasterImage,],
     },
 
 
